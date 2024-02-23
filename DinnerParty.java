0337524f9ccdabfe35a643eb9a13d2cca028b577
BransonWitt <57055648+BransonWitt@users.noreply.github.com>
2024-02-22 21:04:09 -0500

    Update DinnerParty.java

--- DinnerParty.java
+++ DinnerParty.java
@@ -1,9 +1,13 @@
 public class DinnerParty extends Party{
+    //Declaring private int
     private int dinnerChoice = 0;
 
+    //Set method
     public void setChoice(int choice){
         this.dinnerChoice = choice;
     }
+
+    //Get method
     public int getChoice() {
         return this.dinnerChoice;
     }

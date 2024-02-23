3d749cd4daf19700d819b2e975682545396988d0
BransonWitt <57055648+BransonWitt@users.noreply.github.com>
2024-02-22 21:05:08 -0500

    Update Party.java

--- Party.java
+++ Party.java
@@ -1,15 +1,18 @@
 public class Party {
+    //Private variable for the number of Guests
     private int numGuests = 0;
 
+    //Set method for the number of guests
     public void setGuests(int numGuests){
         this.numGuests = numGuests;
     }
+
+    //Get method for the number of guests
     public int getGuests(){
         return this.numGuests;
     }
 
-
-
+    //Updating printing to a return statement to implement GUI
     public String displayInvitation() {
         return("Please come to my party!");
     }
